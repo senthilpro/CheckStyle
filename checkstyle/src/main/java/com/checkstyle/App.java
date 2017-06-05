d187ff16ed3ec4b85845b6d5e2f9942d189dd4ee
senthilpro <ksenthilkumar@prokarma.com>
2017-06-05 12:41:08 +0530

    initial commit

---
+++ checkstyle/src/main/java/com/checkstyle/App.java
@@ -0,0 +1,17 @@
+package com.checkstyle;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public static void main( String[] args )
+	{
+		System.out.println( "Hello World!" );
+	}
+
+	public String getNullValues() {
+		return null;
+	}
+}
